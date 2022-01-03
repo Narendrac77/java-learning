05ce6c73ddde079c46f5b26faf7013550d0628e9
Narendra Chinnakotla <narendrachinnakotla@gmail.com>
2022-01-03 18:55:09 +0530

    simpletree

---
+++ src/com/narendra/java/trees/firstsimpletree/Node.java
@@ -0,0 +1,11 @@
+package com.narendra.java.trees.firstsimpletree;
+
+public class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item){
+        key = item;
+        left = right = null;
+    }
+}
